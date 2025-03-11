16da116980042205505b42d471a239f69980e42a
Артём <temamark2toyota@gmail.com>
2025-03-11 16:54:29 +0700

    Update Model.py

--- Model.py
+++ Model.py
@@ -35,3 +35,45 @@ def div_num(num_list):
         i += 1
     return div
 
+import math
+
+def square_area(a):
+    """Вычисляет площадь квадрата."""
+    return a ** 2
+
+def rectangle_area(a, b):
+    """Вычисляет площадь прямоугольника."""
+    return a * b
+
+def triangle_area(base, height):
+    """Вычисляет площадь треугольника."""
+    return 0.5 * base * height
+
+def circle_area(r):
+    """Вычисляет площадь круга."""
+    return math.pi * r ** 2
+
+def parallelogram_area(base, height):
+    """Вычисляет площадь параллелограмма."""
+    return base * height
+
+def trapezoid_area(a, b, height):
+    """Вычисляет площадь трапеции."""
+    return (a + b) * height / 2
+
+def rhombus_area(d1, d2):
+    """Вычисляет площадь ромба."""
+    return (d1 * d2) / 2
+
+# def area_of_cube(num_list):
+#     area = 6 * (num_list[0]**2)
+#     return area
+#
+# def area_of_box(num_list):
+#     area = 2 * (num_list[0]*num_list[1] + num_list[0]*num_list[2] + num_list[1]*num_list[2])
+#     return area
+#
+# def area_of_box(num_list):
+#     area = 2 * (num_list[0]*num_list[1] + num_list[0]*num_list[2] + num_list[1]*num_list[2])
+#     return area
+
