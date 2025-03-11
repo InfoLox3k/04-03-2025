98a4d45ad385ab363227801f3605cd23f4357d61
Артём <temamark2toyota@gmail.com>
2025-03-11 16:03:14 +0700

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,24 +1,37 @@
 # Model (Модель) - отвечает за данные и логику работы с ними
-def sum_num():
+def sum_num(num_list):
     summ = 0
     for item in num_list:
         summ += item
     return summ
 
-def min_num():
+def min_num(num_list):
     minn = 0
     for item in num_list:
         minn -= item
     return minn
 
-def mult_num():
+def mult_num(num_list):
     multt = 0
+    i = 0
+
     for item in num_list:
-        multt *= item
+        if i == 0:
+            multt = item
+        else:
+            multt *= item
+        i += 1
     return multt
 
-def div_num():
+def div_num(num_list):
     div = 0
+    i = 0
+
     for item in num_list:
-        div /= item
+        if i == 0:
+            div = item
+        else:
+            div /= item
+        i += 1
     return div
+
