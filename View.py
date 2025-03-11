0f0c7ea34d2a4f77b8e5d329a0c3e5984335225c
Артём <temamark2toyota@gmail.com>
2025-03-11 16:02:58 +0700

    Update View.py

--- View.py
+++ View.py
@@ -6,15 +6,15 @@ def display_menu():
     print("\nВыбери действие: 1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление, 0 - выход")
     choice = input("Введи номер действия: ")
 
+    return choice
+
+def Num():
     while True:
         num = float(input('Введите число: '))
         if num == 0:
-            break
-        num_list.append(num)
-
-
-
-    return choice
+            return num_list
+        else:
+            num_list.append(num)
 
 def ZeroDivision():
     print('На ноль делить НЕЛЬЗЯ')
