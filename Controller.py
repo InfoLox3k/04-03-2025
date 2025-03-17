301428cd9b301fd5811e60124a43c67f82eab56f
Артём <temamark2toyota@gmail.com>
2025-03-17 22:45:31 +0700

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -1,5 +1,7 @@
 from Model import *
 from View import *
+from Formula2D import *
+from Formula3D import *
 
 # Controller (Контроллер) - управляет логикой и связывает модель с представлением
 def main():
@@ -36,8 +38,9 @@ def main():
             BeNormal()
 
     elif choice == '5':
-        # try:
+        try:
             choice2 = display_menu_of_area()
+            # Далее условия 2D
             if choice2 == '1':
                 num_list = Num_2D()
                 choice4 = display_menu_of_2D_area()
@@ -55,15 +58,39 @@ def main():
                     result = trapezoid_area(num_list[0], num_list[1], num_list[2])
                 elif choice4 == '7':
                     result = rhombus_area(num_list[0], num_list[1])
-                
-            # elif choice2 == '2':
-            #     choice3 = display_menu_of_3D_area()
-            #     if choice3 == '1':
-            #         lenghth = Num_of_cube_area()
-            #         result = area_of_cube(lenghth)
+
+            # Далее условия 3D
+            elif choice2 == '2':
+                choice3 = display_menu_of_3D_area()
+                if choice3 == '1':
+                    lenghth = Num_of_cube_area()
+                    result = cube_surface_area(lenghth)
+                elif choice3 == '2':
+                    radius = Num_of_circle_area()
+                    result = sphere_surface_area(radius)
+                elif choice3 == '3':
+                    info_list = Num_of_box_area()
+                    result = box_surface_area(info_list[0], info_list[1], info_list[2])
+                elif choice3 == '4':
+                    info_list = Num_of_cylinder_area()
+                    result = cylinder_surface_area(info_list[0], info_list[1])
+                elif choice3 == '5':
+                    info_list = Num_of_cone_area()
+                    result = cone_surface_area(info_list[0], info_list[1])
+                elif choice3 == '6':
+                    info_list = Num_of_pyramid_area()
+                    result = pyramid_surface_area(info_list[0], info_list[1], info_list[2])
+                elif choice3 == '7':
+                    info_list = Num_of_prism_area()
+                    result = prism_surface_area(info_list[0], info_list[1], info_list[2])
             Result(result)
-        # except:
-        #     BeNormal()
+            GroundhogDay = ThatLevelAgain()
+            if GroundhogDay == 'Да':
+                main()
+            else:
+                Goodbye()
+        except:
+            BeNormal()
 
     elif choice == '0':
         Goodbye()
