e9e91fd55e7a4718b2cce8fd390c58c0cf925056
Артём <temamark2toyota@gmail.com>
2025-03-17 22:46:09 +0700

    Update View.py

--- View.py
+++ View.py
@@ -18,7 +18,7 @@ def display_menu_of_2D_area():
     return choice4
 
 def display_menu_of_3D_area():
-    print("\nВыбери действие: 1 - Куб, 2 - Параллелепипед, 3 - Сфера, 4 - Цилиндр, 5 - Конус, 6 - Пирамида, 0 - выход")
+    print("\nВыбери действие: 1 - Куб, 2 - Сфера, 3 - Параллелепипед, 4 - Цилиндр, 5 - Конус, 6 - Пирамида, 7 - Призма, 0 - выход")
     choice3 = input("Введи номер действия: ")
     return choice3
 
@@ -26,30 +26,62 @@ def Num():
     while True:
         num = float(input('Введите число: '))
         if num == 0:
-            return num_list
+            break
         else:
             num_list.append(num)
+    return num_list
 
 def Num_2D():
-    for i in range(3):
-        num = float(input('Введите число: '))
-        if num == 0:
-            return num_list
-        else:
-            num_list.append(num)
 
-# def Num_of_cube_area():
-#     lengh = float(input('Введите длину: '))
-#
-#     return lengh
-#
-# def Num_of_box_area():
-#     lengh = float(input('Введите длину: '))
-#     width = float(input('Введите ширину: '))
-#     height = float(input('Введите высоту: '))
-#     info_list = [lengh, width, height]
-#
-#     return info_list
+    num1 = float(input('Введите длину: '))
+    num2 = float(input('Введите ширину: '))
+    num3 = float(input('Введите высоту: '))
+
+    num_list.append(num)
+    return num_list
+
+def Num_of_cube_area():
+    length = float(input('Введите длину: '))
+    return length
+
+def Num_of_circle_area():
+    radius = float(input('Введите радиус: '))
+    return radius
+
+def Num_of_box_area():
+    length = float(input('Введите длину: '))
+    width = float(input('Введите ширину: '))
+    height = float(input('Введите высоту: '))
+    info_list = [lengh, width, height]
+
+    return info_list
+
+def Num_of_cylinder_area():
+    radius = float(input('Введите радиус: '))
+    height = float(input('Введите высоту: '))
+    info_list = [radius, height]
+    return info_list
+
+def Num_of_cone_area():
+    radius = float(input('Введите радиус: '))
+    length = float(input('Введите длину: '))
+    info_list = [radius, length]
+    return info_list
+
+def Num_of_pyramid_area():
+    A = float(input('Введите площадь основания: '))
+    P = float(input('Введите периметр основания: '))
+    l = float(input('Введите высоту боковой грани: '))
+    info_list = [A, P, l]
+    return info_list
+
+def Num_of_prism_area():
+    A = float(input('Введите площадь основания: '))
+    P = float(input('Введите периметр основания: '))
+    h = float(input('Введите высоту призмы: '))
+    info_list = [A, P, h]
+    return info_list
+
 
 def ZeroDivision():
     print('На ноль делить НЕЛЬЗЯ')
@@ -60,3 +92,7 @@ def BeNormal():
 
 def Result(result):
     print(result)
+
+def ThatLevelAgain():
+    again = input('Продолжим?(Да/Нет): ')
+    return again
